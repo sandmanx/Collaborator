d83280ec0096af5512ba8b33d2180a59393d67f3
piano-man <icm2015003@iiita.ac.in>
2017-03-24 10:58:02 +0530

    video player files added

--- index.js
+++ index.js
@@ -1,18 +1,67 @@
-var app = require('express')();
-var http = require('http').Server(app);
+var google = require('googleapis');
+google.options({proxy: 'http://172.31.1.6:8080/'});
+var express= require('express');
+var bodyParser = require('body-parser');
+var app = express();
+var request = require('request');
+var x;
+var user_name;
+var youtube = google.youtube({ version: 'v3', auth: 'AIzaSyAoN1RLSoqgf7ujPK-2cfT8pz4qQR1_tvg' });
+var http = require('http').createServer(app);
 var io = require('socket.io')(http);
-var port = process.env.PORT || 3000;
 
-app.get('/', function(req, res){
-  res.sendFile(__dirname + '/index.html');
-});
+http.listen(3000);
 
-io.on('connection', function(socket){
-  socket.on('chat message', function(msg){
-    io.emit('chat message', msg);
+app.use(express.static('./Public'));
+app.use(bodyParser.urlencoded({ extended: true }));
+//app.use(bodyParser.json());
+
+app.get('/', function(req,res) {
+    res.sendFile(path);
+ });
+var path = __dirname + '/login.html';
+
+io.on('connection', function (socket) {
+  console.log("user connected");
+  socket.on('pause', function() {
+    console.log("pasue received");
+    io.sockets.emit('pauseAll');
+  });
+
+  socket.on('play', function() {
+    console.log("play recieved");
+    io.sockets.emit('playAll');
+  });
+
+  socket.on('x', function(data) {
+    console.log(data.title + "xxxxx");
+    io.sockets.emit('y', data.title);
+  });
+
+  socket.on('commonPlaylist', function(data) {
+    io.sockets.emit('commonPlaylistReply', data);
   });
 });
 
-http.listen(port, function(){
-  console.log('listening on *:' + port);
+app.post('/index', function(req, res) {
+  user_name = req.body.firstname;
+  res.redirect('Groove 22-03-17.html');
 });
+
+app.get("/user", function(req, res) {
+  res.send(user_name);
+});
+
+app.get('/search/:query', function(req, res) {
+  console.log(req.params);
+  var results = youtube.search.list({part : 'id,snippet', q: req.params.query, maxResults: 25});
+  x = results.url.href;
+  request({url: x, json: true, proxy :'http://172.31.1.6:8080/'}, function(err, localres, json) {
+    if (err) {
+      throw err;
+    }
+    res.send(json.items[0].id.videoId);
+  });
+});
+
+
