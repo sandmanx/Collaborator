330952103960f472ed3201bd67d8743dc6110889
piano-man <icm2015003@iiita.ac.in>
2017-03-27 11:40:03 +0530

    indentation added

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 var google = require('googleapis');
-google.options({proxy: 'http://172.31.1.6:8080/'});
-var express= require('express');
+google.options({ proxy: 'http://172.31.1.3:8080/' });
+var express = require('express');
 var bodyParser = require('body-parser');
 var app = express();
 var request = require('request');
@@ -17,49 +17,47 @@ app.use(express.static('./Public'));
 app.use(bodyParser.urlencoded({ extended: true }));
 //app.use(bodyParser.json());
 
-app.get('/', function(req,res) {
-    res.sendFile(path);
- });
- app.get('/logout',function(req,res){
-   console.log("blog");
-   res.redirect('http://localhost:3000/login.html');
- })
+app.get('/', function (req, res) {
+  res.sendFile(path);
+});
+app.get('/logout', function (req, res) {
+  console.log("blog");
+  res.redirect('http://localhost:3000/login.html');
+})
 var path = __dirname + '/login.html';
 
 io.on('connection', function (socket) {
   console.log("user connected");
-  socket.on('pause', function() {
+  socket.on('pause', function () {
     console.log("pasue received");
     io.sockets.in(socket.room).emit('pauseAll');
   });
 
-  socket.on('play', function() {
+  socket.on('play', function () {
     console.log("play recieved");
     io.sockets.in(socket.room).emit('playAll');
   });
 
-  socket.on('x', function(data) {
+  socket.on('x', function (data) {
     console.log(data.title + "xxxxx");
     io.sockets.in(socket.room).emit('y', data.title);
   });
-  
 
-  socket.on('commonPlaylist', function(data) {
+
+  socket.on('commonPlaylist', function (data) {
     io.sockets.in(socket.room).emit('commonPlaylistReply', data);
   });
-  socket.on('leaveroom',function(room)
-  {
+  socket.on('leaveroom', function (room) {
     console.log(room)
     socket.room = undefined;
     console.log(socket.room);
     socket.leave(room);
   });
 
-  socket.on('chat message', function(msg){
+  socket.on('chat message', function (msg) {
     io.sockets.in(socket.room).emit('chat message', msg);
   });
-  socket.on('room',function(room)
-  {
+  socket.on('room', function (room) {
     console.log(socket.room);
 
     //if(socket.room === undefined)
@@ -75,20 +73,20 @@ io.on('connection', function (socket) {
   })
 });
 
-app.post('/index', function(req, res) {
+app.post('/index', function (req, res) {
   user_name = req.body.firstname;
   res.redirect('finaltry.html');
 });
 
-app.get("/user", function(req, res) {
+app.get("/user", function (req, res) {
   res.send(user_name);
 });
 
-app.get('/search/:query', function(req, res) {
+app.get('/search/:query', function (req, res) {
   console.log(req.params);
-  var results = youtube.search.list({part : 'id,snippet', q: req.params.query, maxResults: 25});
+  var results = youtube.search.list({ part: 'id,snippet', q: req.params.query, maxResults: 25 });
   x = results.url.href;
-  request({url: x, json: true, proxy :'http://172.31.1.6:8080/'}, function(err, localres, json) {
+  request({ url: x, json: true, proxy: 'http://172.31.1.3:8080/' }, function (err, localres, json) {
     if (err) {
       throw err;
     }
